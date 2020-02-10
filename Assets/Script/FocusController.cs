c27d666f4f734d74b1a0dded50c9fe03cb23100d
EngKyohio <msmk2020@outlook.jp>
2020-02-10 21:00:28 +0900

    Script:FocusController を追加

---
+++ Assets/Script/FocusController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusController : MonoBehaviour
+{
+    public GameObject mainCamera;              //メインカメラ格納用
+    public GameObject playerObject;            //回転の中心となるプレイヤー格納用
+    public GameObject lookPoint;
+    public float rotateSpeed = 2.0f;            //回転の速さ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotateCameraの呼び出し
+        MoveFocus();
+    }
+
+    //カメラを回転させる関数
+    private void MoveFocus()
+    {
+        //Vector3でX,Y方向の回転の度合いを定義
+        Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed, Input.GetAxis("Mouse Y") * rotateSpeed, 0);
+
+        //transform.RotateAround()をしようしてメインカメラを回転させる
+        lookPoint.transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
+        lookPoint.transform.RotateAround(playerObject.transform.position, Vector3.right, -angle.y);
+
+        mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
+        mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.right, -angle.y);
+
+        mainCamera.transform.LookAt(lookPoint.transform);
+    
+        //mainCamera.transform.eulerAngles += new Vector3(-angle.y, angle.x);
+    }
+}
