36c8dec0ada15119ec907baacc9193c6c55c7bad
doppio96 <55580720+doppio96@users.noreply.github.com>
2020-02-12 16:24:39 +0900

    treasureスクリプト作成
    treasureスクリプト
    Goldマテリアル作成

---
+++ Assets/Script/Treasure.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : MonoBehaviour
+{
+    GameObject clickedGameObject;
+    public int value;
+    public string tresureName;
+    //public Scorebord scorebord;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //UpdateScore(scorebord.Score());
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                clickedGameObject = hit.collider.gameObject;
+                
+                this.gameObject.SetActive(false);
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+    }
+
+    void UpdateScore(int score)
+    {
+        score = score + value;
+        return;
+    }
+}
