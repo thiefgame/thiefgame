890fc63397fc8a7385268c6c89f8ff14b1eb84b0
EngKyohio <msmk2020@outlook.jp>
2020-02-13 19:24:06 +0900

    Unityちゃんに合わせて FocusController の微調整

--- Assets/Script/FocusController.cs
+++ Assets/Script/FocusController.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class FocusController : MonoBehaviour
 {
-    public GameObject mainCamera;              //メインカメラ格納用
-    public GameObject toggle;            //回転の中心となるプレイヤー格納用
-    public GameObject lookPoint;
-    public float rotateSpeed = 2.0f;            //回転の速さ
-    public float lookUpLimit = 40;
-    public float lookDownLimit = 30;
+    public GameObject mainCamera;              //メインカメラ
+    public GameObject toggle;                  //カメラと注視点の回転軸
+    public GameObject lookPoint;               //注視点
+    public GameObject player;                  //プレイヤー
+    public float rotateSpeed = 2.0f;           //回転の速さ
+    public float lookUpLimit = 40;             //見上げる角度制限(度)
+    public float lookDownLimit = 30;           //見下ろす角度制限(度)
 
     private Vector3 firstCamPos;
     private Vector3 firstLookPos;
+    private Vector3 lastTogglePos;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        firstCamPos = mainCamera.transform.position;
-        firstLookPos = lookPoint.transform.position;
+        lastTogglePos = toggle.transform.position;
+        firstCamPos = lastTogglePos - mainCamera.transform.position;
+        firstLookPos = lastTogglePos - lookPoint.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 toggleMovement = toggle.transform.position - lastTogglePos;
+        if (toggleMovement != Vector3.zero)
+        {
+            mainCamera.transform.position += toggleMovement;
+            lookPoint.transform.position += toggleMovement;
+        }
+
         //rotateCameraの呼び出し
         if (Input.GetMouseButton(1))
         {
@@ -32,6 +42,9 @@ public class FocusController : MonoBehaviour
         }
 
         if (Input.GetKeyDown(KeyCode.R)) { ResetFocus(); }
+        //else { ChasePlayer(); }
+
+        lastTogglePos = toggle.transform.position;
     }
 
     //カメラを回転させる関数
@@ -63,7 +76,6 @@ public class FocusController : MonoBehaviour
                 mainCamera.transform.RotateAround(toggle.transform.position, mainCamera.transform.right, -angle.y);
             }
         }
-        Debug.Log("" + mainCamera.transform.eulerAngles.x);
 
         mainCamera.transform.LookAt(lookPoint.transform);
     
@@ -73,9 +85,21 @@ public class FocusController : MonoBehaviour
     //視点リセット
     private void ResetFocus()
     {
-        lookPoint.transform.position = firstLookPos;
+        //toggle.transform.rotation = Quaternion.Euler(player.transform.forward);
+        /*lookPoint.transform.position = toggle.transform.position - Vector3.Scale(firstLookPos, player.transform.forward.normalized);
         lookPoint.transform.rotation = Quaternion.identity;
-        mainCamera.transform.position = firstCamPos;
+        mainCamera.transform.position = toggle.transform.position - Vector3.Scale(firstCamPos, player.transform.forward.normalized);*/
+        lookPoint.transform.position = toggle.transform.position - firstLookPos.magnitude * -player.transform.forward.normalized;
+        lookPoint.transform.rotation = Quaternion.identity;
+        mainCamera.transform.position = toggle.transform.position - firstCamPos.magnitude * player.transform.forward.normalized;
         mainCamera.transform.LookAt(lookPoint.transform);
+    }
+
+    private void ChasePlayer()
+    {
+        lookPoint.transform.position = toggle.transform.position - Vector3.Scale(firstLookPos, mainCamera.transform.forward.normalized);
+        lookPoint.transform.rotation = Quaternion.identity;
+        mainCamera.transform.position = toggle.transform.position - Vector3.Scale(firstCamPos, mainCamera.transform.forward.normalized);
+        //mainCamera.transform.LookAt(lookPoint.transform);
     }
 }
