df0545fbf477a9b163f3ef32d81033e9486bb549
doppio96 <55580720+doppio96@users.noreply.github.com>
2020-02-20 20:44:15 +0900

    ポーズメニューをキー操作に修正

--- Assets/Script/GoalUI.cs
+++ Assets/Script/GoalUI.cs
@@ -2,20 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GoalUI : MonoBehaviour
 {
     [SerializeField] GameObject GoalButton;
-  
+    [SerializeField] GameObject continueButton;
+    Button button;
+   
+    void Start()
+    {
+        button =continueButton.GetComponent<Button>();
+        //ボタンが選択された状態になる
+        button.Select();
+    }
+    
     //プレイヤーがゴール地点に入ったとき
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             //クリックしたときゴールボタンを表示して時を止める（FixedUpdateメソッドのみ）
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetKeyDown(KeyCode.G))
             {
                 GoalButton.SetActive(true);
+
+                button = continueButton.GetComponent<Button>();
+                //ボタンが選択された状態になる
+                button.Select();
                 Time.timeScale = 0f;
             }
         }
@@ -27,6 +41,8 @@ public class GoalUI : MonoBehaviour
         //ポーズを解除してボタンを非表示
         GoalButton.SetActive(false);
         Time.timeScale = 1f;
+
+        Cursor.visible = true;
     }
 
     public void OnClickEscape()
