958cb95a5c9c08be05ee0cc0f9ba8d39bcd76193
LightToRingo <lightblooooo@gmail.com>
2020-02-29 20:42:05 +0900

    カギの追加と扉の制御用スクリプトの追加

--- Assets/Member(ikeda)/KeyController.cs
+++ Assets/Member(ikeda)/KeyController.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class KeyController : MonoBehaviour
 {
     bool isNear;
+    Animator animator;
+    //private GameObject goldKeyIcon;
+    private GameObject silverKeyIcon;
 
+    private void Awake()
+    {
+        //goldKeyIcon = GameObject.Find("GoldKeyIcon");
+        
+        silverKeyIcon = GameObject.Find("SilverKeyIcon");
+        //goldKeyIcon = GameObject.Find("GoldKeyIcon");
+        silverKeyIcon.SetActive(false);
+        //goldKeyIcon.SetActive(false);
+    }
     // Start is called before the first frame update
     void Start()
     {
         isNear = false;
-        this.gameObject.SetActive(true);
     }
-
     // Update is called once per frame
     void Update()
     {
         if (isNear && Input.GetKeyDown("e"))
         {
             this.gameObject.SetActive(false);
+            /*if (this.gameObject.name == "gold_key_searchArea")
+            {
+                goldKeyIcon.SetActive(true);
+                
+            }*/
+            if(this.gameObject.name == "silver_key_searchArea")
+            {
+                silverKeyIcon.SetActive(true);
+            }
         }
     }
 
@@ -27,6 +46,13 @@ public class KeyController : MonoBehaviour
         if(other.tag == "Player")
         {
             isNear = true;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown("e"))
+        {
+            other.GetComponent<Animator>().SetTrigger("Steal");
         }
     }
 
--- Assets/Member(ikeda)/LockedDoor.cs
+++ Assets/Member(ikeda)/LockedDoor.cs
@@ -8,33 +8,82 @@ public class LockedDoor : MonoBehaviour
     public AudioClip closeClip;
     public AudioClip lockedClip;
     //GameObjects
-    GameObject key;
     public Animator animator;
-
+    //近づいたらとぅっとぅるーになるフラグ
     bool isNear;
+    //金のカギか銀のカギか判定させるフラグ 1ならゴールドドア 2ならシルバードア
+    int whichOne;
+
 
 
     void Start()
     {
         isNear = false;
+        whichOne = 0;
         animator = transform.parent.GetComponent<Animator>();
-        key = GameObject.Find("golden_key");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("e") && isNear && key.activeSelf == false)
+
+        if (Input.GetKeyDown("e") && isNear && whichOne == 2 && GameObject.Find("SilverKeyIcon"))
         {
             animator.SetBool("Open", !animator.GetBool("Open"));
             if (animator.GetBool("Open"))
             {
-                //音の再生 displayMessage 開いた
+                //開いたときの音の再生
+                gameObject.GetComponent<AudioSource>().PlayOneShot(openClip);
             }
-            else
+            if (!animator.GetBool("Open"))
             {
-                //音の再生 displayMessage 開かない
+                //閉めたときの音の再生
+                gameObject.GetComponent<AudioSource>().PlayOneShot(closeClip);
             }
+        } 
+        /*else if(Input.GetKeyDown("e") && isNear && GameObject.Find("silver_key").activeSelf == true && whichOne == 2)
+        {
+            animator.SetBool("Open", !animator.GetBool("Open"));
+            if (animator.GetBool("Open"))
+            {
+                //開いたときの音の再生
+                gameObject.GetComponent<AudioSource>().PlayOneShot(openClip);
+            }
+            if (!animator.GetBool("Open"))
+            {
+                //閉めたときの音の再生
+                gameObject.GetComponent<AudioSource>().PlayOneShot(closeClip);
+            }
+        }*/
+        else
+        {
+            //カギを持っていないときのおとの再生
+            gameObject.GetComponent<AudioSource>().PlayOneShot(lockedClip);
         }
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown("e"))
+        {
+            other.GetComponent<Animator>().SetTrigger("Steal");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && this.gameObject.name == "SearchAreaGold")
+        {
+            isNear = true;
+            whichOne = 1;
+        }
+        if(other.tag == "Player" && this.gameObject.name == "SearchAreaSilver")
+        {
+            isNear = true;
+            whichOne = 2;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isNear = false;
     }
 }
