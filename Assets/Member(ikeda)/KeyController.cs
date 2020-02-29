f2220aa31acfb5fe24c8aad3c0d2d2d8433391ff
LightToRingo <lightblooooo@gmail.com>
2020-02-29 16:47:39 +0900

    added key

---
+++ Assets/Member(ikeda)/KeyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    bool isNear;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isNear = false;
+        this.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNear && Input.GetKeyDown("e"))
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isNear = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isNear = false;
+    }
+}
---
+++ Assets/Member(ikeda)/LockedDoor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+    public AudioClip openClip;
+    public AudioClip closeClip;
+    public AudioClip lockedClip;
+    //GameObjects
+    GameObject key;
+    public Animator animator;
+
+    bool isNear;
+
+
+    void Start()
+    {
+        isNear = false;
+        animator = transform.parent.GetComponent<Animator>();
+        key = GameObject.Find("golden_key");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("e") && isNear && key.activeSelf == false)
+        {
+            animator.SetBool("Open", !animator.GetBool("Open"));
+            if (animator.GetBool("Open"))
+            {
+                //音の再生 displayMessage 開いた
+            }
+            else
+            {
+                //音の再生 displayMessage 開かない
+            }
+        }
+    }
+}
