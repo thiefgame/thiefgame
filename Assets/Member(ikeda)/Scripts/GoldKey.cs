ec45670156ac27db5b3ce16055783a0fba924792
LightToRingo <lightblooooo@gmail.com>
2020-03-03 18:15:11 +0900

    各スクリプトの調整

---
+++ Assets/Member(ikeda)/Scripts/GoldKey.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldKey : MonoBehaviour
+{
+{
+    bool isNear;
+    Animator animator;
+    //private GameObject goldKeyIcon;
+    private GameObject goldKeyIcon;
+
+    void Start()
+    {
+        isNear = false;
+        goldKeyIcon = GameObject.Find("SilverKeyIcon");
+        goldKeyIcon.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    //カギを入手するときの動作
+    {
+        if (isNear && Input.GetKeyDown("e"))
+        {
+            Destroy(this.gameObject);
+            /*if (this.gameObject.name == "gold_key_searchArea")
+            {
+                goldKeyIcon.SetActive(true);            
+                
+            }*/
+            if (this.gameObject.name == "gold_key_searchArea")
+            {
+                goldKeyIcon.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isNear = true;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown("e"))
+        {
+            other.GetComponent<Animator>().SetTrigger("Steal");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isNear = false;
+    }
+}
